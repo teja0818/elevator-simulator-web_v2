07b96b141e889f090ec5293f392aec84d62c0f90
teja0418 <t.sivateja@live.com>
2025-04-07 12:47:15 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Elevator Simulator 🚀</h1>
+    </div>
+  );
+}
+export default App;
